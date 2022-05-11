cd40e757f8f9ce2146d35de850a26a7be167c2a1
shreyasb20 <32397581+ShreyasBhaktharam@users.noreply.github.com>
2022-05-11 09:06:12 +0530

    Add files via upload

---
+++ Lab 5/quad_generation.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "quad_generation.h"
+
+void quad_code_gen(char* a, char* b, char* op, char* c)
+{
+	//use fprintf to output the quadruple code to icg_quad_file
+	fprintf(icg_quad_file, "%s\t %s\t %s\t %s\n", op, b, c, a);
+}
+
+char* new_temp()
+{
+		//returns a pointer to a new temporary
+		//variable name
+		char *temp = (char*)malloc(sizeof(char)*10);
+		sprintf(temp, "t%d", temp_no);
+		temp_no++;
+		return temp;
+}
